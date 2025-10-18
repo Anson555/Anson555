0b12cf6687dac694c69f9bfb530bf367dfe5c4b4
Anson555 <myipinghei112730@gmail.com>
2025-10-18 03:19:59 +0000

    update

---
+++ my-first-worker/src/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		return new Response('https://symmetrical-barnacle-7vp6rpr4p6wphp494-6054.app.github.dev/');
+	},
+} satisfies ExportedHandler<Env>;
+0
